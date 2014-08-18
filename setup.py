4de184a41856a78a164c488e42aaaa4f382b5cc9
canard0328 <ryota.kamoshida@gmail.com>
2014-08-18 23:38:50 +0900

    modify

--- setup.py
+++ setup.py
@@ -11,11 +11,14 @@ URL = ""
 VERSION = __import__(PACKAGE).__version__
 LICENSE = __import__(PACKAGE).__license__
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.rst"),
+    long_description=long_description,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license=LICENSE,

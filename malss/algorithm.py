a4728e7c3e8334d8de7d4fecafcd2c8ee51eeefb
canard0328 <ryota.kamoshida@gmail.com>
2014-08-25 23:54:47 +0900

    add gitignore

---
+++ malss/algorithm.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class Algorithm(object):
+    def __init__(self, estimator, parameters):
+        self.estimator = estimator
+        self.parameters = parameters
--- malss/malss.py
+++ malss/malss.py
@@ -11,6 +11,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from docutils.core import publish_cmdline
+from sklearn.datasets import fetch_mldata
+
+from algorithm import Algorithm
 
 
 class MALSS(object):
@@ -41,7 +44,7 @@ class MALSS(object):
             self.X = StandardScaler().fit_transform(self.X)
             self.task = task
             self.n_jobs = n_jobs
-            self.algorithms, self.tuned_parameters = self.__choose_algorithm()
+            self.algorithms = self.__choose_algorithm()
         elif task == 'regression':
             raise ValueError('task:%s is not implemented yet')
         else:
@@ -50,12 +53,11 @@ class MALSS(object):
 
     def __choose_algorithm(self):
         algorithms = []
-        tuned_parameters = []
         if self.task == 'classification':
-            algorithms.append(SVC(random_state=0))
-            tuned_parameters.append([{'kernel': ['rbf'], 'C': [1, 10, 100, 1000],
-                'gamma': [1e-4, 1e-3, 1e-2, 1e-1]}])
-        return algorithms, tuned_parameters
+            algorithms.append(Algorithm(SVC(random_state=0),
+                [{'kernel': ['rbf'], 'C': [1, 10, 100, 1000],
+                'gamma': [1e-4, 1e-3, 1e-2, 1e-1]}]))
+        return algorithms
     
     
     def execute(self):
@@ -71,20 +73,17 @@ class MALSS(object):
             sc = 'r2'
         
         for i in xrange(len(self.algorithms)):
-            estimator = self.algorithms[i]
-            parameters = self.tuned_parameters[i]
+            estimator = self.algorithms[i].estimator
+            parameters = self.algorithms[i].parameters
             cv = cross_validation.StratifiedShuffleSplit(self.y, test_size=0.2, random_state=0)
             clf = GridSearchCV(estimator, parameters, cv=cv, scoring=sc, n_jobs=self.n_jobs)
             clf.fit(self.X, self.y)
-            self.algorithms[i] = clf.best_estimator_
+            self.algorithms[i].estimator = clf.best_estimator_
 
 
     def __plot_learning_curve(self):
-        for estimator in self.algorithms:
-            plt.figure()
-            plt.title(estimator.__class__.__name__)
-            plt.xlabel("Training examples")
-            plt.ylabel("Score")
+        for alg in self.algorithms:
+            estimator = alg.estimator
             cv = cross_validation.StratifiedShuffleSplit(self.y, test_size=0.2, random_state=0)
             train_sizes, train_scores, test_scores = learning_curve(
                 estimator, self.X, self.y, cv=cv, n_jobs=self.n_jobs)
@@ -92,6 +91,11 @@ class MALSS(object):
             train_scores_std = np.std(train_scores, axis=1)
             test_scores_mean = np.mean(test_scores, axis=1)
             test_scores_std = np.std(test_scores, axis=1)
+
+            plt.figure()
+            plt.title(estimator.__class__.__name__)
+            plt.xlabel("Training examples")
+            plt.ylabel("Score")
             plt.grid()
         
             plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
@@ -105,7 +109,7 @@ class MALSS(object):
                      label="Cross-validation score")
         
             plt.legend(loc="lower right")
-            plt.savefig('learning_curve_%s.png'%estimator.__class__.__name__, bbox_inches='tight', dpi=50)
+            plt.savefig('learning_curve_%s.png'%estimator.__class__.__name__, bbox_inches='tight', dpi=75)
             plt.close()
     
     
@@ -116,10 +120,45 @@ class MALSS(object):
         publish_cmdline(writer_name='html', argv=['report.rst', 'report.html'])
 
 
-if __name__=="__main__":
+def __make_data():
+    import os
+    
+    if not os.path.exists('data/spam.txt'):
+        import urllib
+        urllib.urlretrieve('http://mldata.org/repository/data/download/uci-20070111-spambase/', 'data/uci-20070111-spambase.arff')
+        fi = open('data/uci-20070111-spambase.arff', 'r')
+        fo = open('data/spam.txt', 'w')
+        header = []
+        for line in fi:
+            if '@attribute' in line:
+                header.append(line.rstrip().split(' ')[1])
+            if '@data' in line:
+                break
+        for h in header:
+            fo.write(h)
+            if h == header[-1]:
+                fo.write('\n')
+            else:
+                fo.write(',')
+        for line in fi:
+            line = line.rstrip()
+            line = line[:-1]+'spam' if line[-1] == '1' else line[:-1]+'nonspam'
+            fo.write(line+'\n')
+        fi.close()
+        fo.close()
+        
+        os.remove('data/uci-20070111-spambase.arff')
+
     data = pd.read_csv('./data/spam.txt', header=0)
-    y = data['type']
-    del data['type']
+    y = data['class']
+    del data['class']
+    
+    return data, y
+
+
+if __name__=="__main__":
+    data, y = __make_data()
     cls = MALSS(data, y, 'classification', n_jobs=3)
     cls.execute()
+
 

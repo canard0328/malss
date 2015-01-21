3a5d633a3ae7d68bb6320fc5b02dfb70d928b4fc
canard0328 <ryota.kamoshida@gmail.com>
2015-01-22 00:08:28 +0900

    fix bugs

--- malss/__init__.py
+++ malss/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Ryota KAMOSHIDA'
-__version__ = '0.4.7'
+__version__ = '0.4.8'
 __license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
 
 # high level interface

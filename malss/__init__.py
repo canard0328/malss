6be184ba27bd8ef9b9a95644432fbd7ecedce678
鴨志田亮太 <ryota-k@kamoshidaryouta-no-MacBook-Pro.local>
2016-01-24 23:37:25 +0900

    test2

--- malss/__init__.py
+++ malss/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 __author__ = 'Ryota KAMOSHIDA'
 __version__ = '1.0.0'
 __license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'

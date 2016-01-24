8ac671545eb255c36b5d352607bd9604e0dc3168
鴨志田亮太 <ryota-k@kamoshidaryouta-no-MacBook-Pro.local>
2016-01-24 20:11:08 +0900

    bug fix

--- malss/__init__.py
+++ malss/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Ryota KAMOSHIDA'
 __version__ = '1.0.0'
 __license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
 
-__all__ = ['algorithm', 'data', 'malss']
+#__all__ = ['algorithm', 'data', 'malss']
 
 # high level interface
-from malss import MALSS
+#from malss import MALSS

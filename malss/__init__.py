4a3a837aaecdab32eaad8e3e3ff5cd6adfecc529
canard0328 <ryota.kamoshida@gmail.com>
2018-12-01 18:22:35 +0900

    version up

--- malss/__init__.py
+++ malss/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Ryota KAMOSHIDA'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
 
 

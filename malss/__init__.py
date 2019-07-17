1f4009203a7b2e84257f50b9b55d63035b939495
鴨志田亮太 / KAMOSHIDA，RYOTA <ryota.kamoshida.vw@hitachi.com>
2019-07-17 23:01:56 +0900

    fix a bug

--- malss/__init__.py
+++ malss/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Ryota KAMOSHIDA'
-__version__ = '2.2.5'
+__version__ = '2.2.6'
 __license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
 
 
